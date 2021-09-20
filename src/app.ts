57040b4379952583e0502915a58539e7ccf15ed5
Iyanu Adelekan <iyanuadelekan@gmail.com>
2021-09-20 15:09:16 +0100

    feat: setup server to use helmet

--- src/app.ts
+++ src/app.ts
@@ -2,12 +2,14 @@ import express from 'express';
 import swaggerUI from 'swagger-ui-express';
 import YAML from 'yamljs';
 import rideRouter from './api/router/rideRouter';
+import helmet from 'helmet';
 
 const app = express();
 
 const swaggerDoc = YAML.load('./docs/swagger.yml');
 
 export default () => {
+  app.use(helmet());
   app.use('/documentation', swaggerUI.serve, swaggerUI.setup(swaggerDoc));
   app.use('/rides', rideRouter);
   app.get('/health', (_req, res) => res.send('Healthy'));
